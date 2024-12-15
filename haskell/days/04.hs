f9c9ecd4daaec7fa0f9eb7bf121eccc18179efe7
Ptkyr <ptkyr@ptkyr>
2024-12-14 19:32:40 -0500

    day4

---
+++ haskell/days/04.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import Arrays
+import Mayn
+import Parsing
+import Utils
+
+$(generateMain "04")
+
+aocParse :: Parser (Arr2D Char)
+aocParse = listArr2D1 <$> (some upperChar) `endBy` newline <* eof
+
+partOne :: Arr2D Char -> Int
+partOne arr =
+  sum
+    . map
+      ( \(i, e) ->
+          if e /= 'X'
+            then 0
+            else
+              ( countIf xmas $
+                  lineSearches i
+              )
+      )
+    $ assocs arr
+  where
+    xmas :: Coords -> Bool
+    xmas coords = (== map Just "XMAS") $ map (at arr) coords
+    lineSearches :: Coord -> [Coords]
+    lineSearches c = map (fillLine c) $ fanEnds c
+    fanEnds :: Coord -> Coords
+    fanEnds c =
+      zipWith (liftT2 (+)) (repeat c) $
+        [(-3, -3), (-3, 0), (-3, 3), (0, -3), (0, 3), (3, -3), (3, 0), (3, 3)]
+
+partTwo :: Arr2D Char -> Int
+partTwo arr =
+  length
+    . filter
+      ( \(i, e) ->
+          e == 'A' && (xmas i)
+      )
+    $ assocs arr
+  where
+    xmas :: Coord -> Bool
+    xmas (x, y) =
+      (sammas $ fillLine (x - 1, y - 1) (x + 1, y + 1))
+        && (sammas $ fillLine (x - 1, y + 1) (x + 1, y - 1))
+      where
+        sammas = cOr (== map Just "SAM") (== map Just "MAS") . map (at arr)
